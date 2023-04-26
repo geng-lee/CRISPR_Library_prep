5c6ff04e15308c6e6c1cdfdf6ae88d84118fb339
VChapdelaineT <96508889+VChapdelaineT@users.noreply.github.com>
2023-04-26 11:56:23 -0400

    Update oligomer.py

--- oligomer.py
+++ oligomer.py
@@ -13,22 +13,16 @@ argparser = argparse.ArgumentParser(
     description='This Software is part of a tool to design Guiding RNA. \n This script helps design oligomer/concatemere for array Crispr experiment ')
 argparser.add_argument('-S', '--Study', metavar='name',
                        dest='Guide', type=str, required=True, help='Guide Sequence')
-argparser.add_argument('-I', '--iStop', metavar='name',
-                       dest='iStop', type=str, required=True, help='iStop Positive Controls')
-argparser.add_argument('-V', '--iStopVep', metavar='name',
-                       dest='Vep', type=str, required=True, help='Variant effect predictor associated with Positive control library')
-argparser.add_argument('-K', '--N_iStop', metavar='integer',
-                       dest='NiStop', type=int, required=True, help='Number of Positive controls')
+argparser.add_argument('-P', '--Positive', metavar='file',
+                       dest='Vep', type=str, required=True, nargs='*', help='Variant effect predictor associated with Positive control library')
 argparser.add_argument('-N', '--Negative', metavar='name',
-                       dest='Negative', type=str, required=True, help='Negative controls ')
-argparser.add_argument('-J', '--N_Negative', metavar='integer',
-                       dest='N_negative', type=int, required=True, help='number of negative controls')
+                       dest='Negative', type=str, required=True, nargs='*', help='Negative controls ')
 argparser.add_argument('-E','--Exclusion', metavar = 'file', dest = 'Exclusion', type = str, required = False,default=None, help = 'List of guides to be excluded')
 argparser.add_argument('-O', '--out', metavar='file', dest='Output',
                        type=str, required=False, default='out', help='Ouput file name')
 argparser.add_argument('--BSMBI', metavar='file', dest='BSMBI',
                        type=str, required=False, default='CGTCTC', help='Restriction Site')
-argparser.add_argument('-P', '--PRIMER_FILE', metavar='file',
+argparser.add_argument('-p', '--PRIMER_FILE', metavar='file',
                        dest='PRIMER', type=str, required=False, help='Primer File')
 argparser.add_argument('-f', '--forward_primer', metavar=int, dest='Primer_pair', type=int,
                        required=False, default=50, help='Designation of the primer pair')
@@ -57,7 +51,7 @@ def Assert_Fragments_BSMBI(BSMBI, fragments):
 
 def filter_restriction_sgrna (df) :
     for index, row in df.iterrows() :
-        test_restriction_sgrna=args.fragment[0] + row.gRNA_seq +args.fragment[1]
+        test_restriction_sgrna=args.fragment[0] + row.Protospacer +args.fragment[1]
             if (test_restriction_sgrna.count(args.BSMBI) +  test_restriction_sgrna.count(BSMBI_reverse))>3:
                 df.drop(index=index, inplace=True)
 
@@ -73,26 +67,42 @@ if __name__ == '__main__':
     primers_reverse = primers.loc[args.Primer_pair]['Reverse.Binding']
     primers_forward = primers.loc[args.Primer_pair]['SKPP.forward.Seq']
     # process CRISPOR file
-    vep=pd.read_csv(args.Vep, sep='\t',index_col='#Uploaded_variation',skiprows=90)
-    Stoppers=vep[vep.Consequence=='stop_gained'].index
-    istop = pd.read_csv(args.iStop, sep=',',index_col='ID').loc[Stoppers].sample(n=args.NiStop,random_state=11)
-    negative = pd.read_csv(args.Negative, sep=',',index_col='ID').sample(n=args.N_negative,random_state=11)
-    guides=pd.read_csv(args.Guide, sep=',',index_col='ID')
-    guides.drop_duplicates(subset='gRNA_seq_POSstrand', inplace=True, keep=False)
-    guides=pd.concat([guides,istop,negative])
+    positive_options_df=pd.DataFrame([args.Vep[(0+i):(2+i)] for i in range(0,len(args.Vep),3)],columns = ['file', 'N','Positive_Type'])
+    positive_tot_df=pd.DataFrame([],columns=['ID','Protospacer'])
+    for index, row in positive_options_df.iterrows():
+        vep=pd.read_csv(row.file, sep='\t',comment='#')
+        Control_positive_df=vep[vep.Impact==row.Positive_Type]
+        filter_restriction_sgrna(negative_df)
+        positive_tot_df.append(Control_positive_df[['ID','Protospacer']].sample(n=row.N,random_state=11))
+    negative_df=pd.DataFrame([args.Vep[(0+i):(2+i)] for i in range(0,len(args.Vep),2)],columns = ['file', 'N'])
+    negative_tot_df=pd.DataFrame([],columns=['ID','Protospacer'])
+    negative_unused_df=pd.DataFrame([],columns=['ID','Protospacer'])
+    for index, row in Negative_df.iterrows():
+        negative_df = pd.read_csv(row.file, sep=',',index_col='ID',comment='#')
+        filter_restriction_sgrna(negative_df)
+        negative_ran_df.sample(n=row.N,random_state=11)
+        negative_tot_df.append(negative_df[['ID','Protospacer']])
+        negative_unused_df.append(negative_df.loc[[not (i in negative_ran_df.ID) for i in negative_df.ID],['ID','Protospacer']])
+    Study=pd.read_csv(args.Guide, sep=',',index_col='ID',comment='#')
+    Study.drop_duplicates(subset='Protospacer', inplace=True, keep=False)
+    filter_restriction_sgrna(Study)
+    guides=pd.concat([Study[['ID','Protospacer']],positive_tot_df,Negative_tot_df])
     if args.Exclusion :
         exclusion=pd.read_csv(args.Exclusion, sep='\t')
-        guides=guides.loc[guides['targetSeq'].isin(excludes[0].tolist())]
-    filter_restriction_sgrna(guides)
-    guides= guides.sample(frac=1,random_state=11)
+    guides=guides.loc[guides['Protospacer'].isin(excludes[0].tolist())]
+    guides.drop_duplicates(subset='Protospacer', inplace=True, keep=False)
     SguidePerConcat = len(args.fragments)-1
+    while len(guides) % SguidePerConcat !=0 :
+        guides.append(negative_unused_df.sample(frac=1,random_state=11).iloc[0:len(guides) % SguidePerConcat])
+        guides.drop_duplicates(subset='Protospacer', inplace=True, keep=False)
+    guides= guides.sample(frac=1,random_state=11)
     with open(args.Output, 'w') as out:
-        for i in range(0, len(guides.index) // SguidePerConcat):
+        for i in range(0, len(guides.index), SguidePerConcat):
             GuidesNames = []
             concat = [primers_forward]
             for j in range(0, SguidePerConcat):
-                concat.extend([args.fragments[j], guides.iloc[i * SguidePerConcat+j].targetSeq])
-                GuidesNames.append(guides.iloc[i*SguidePerConcat+j].label)
+                concat.extend([args.fragments[j], guides.iloc[i+j].Protospacer])
+                GuidesNames.append(guides.iloc[i+j].ID)
             Names = ','.join(GuidesNames)
             concat.extend([args.fragments[SguidePerConcat],primers_reverse])
             concatemere = ''.join(concat)
